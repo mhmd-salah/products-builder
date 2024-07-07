31e7dc826d682e22691b1ff1fb58ac81a5ede2ee
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-07-07 06:30:29 +0200

    enhance modal ui

--- src/components/ui/Modle.tsx
+++ src/components/ui/Modle.tsx
@@ -1,4 +1,4 @@
-import {  Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import {  Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
 import { IPropsModle } from "../../interfaces";
 // import { useState } from "react";
 
@@ -13,26 +13,22 @@ export default function Modle({isOpen,close,title,children}:IPropsModle) {
         open={isOpen}
         as="div"
         className="relative z-10 focus:outline-none text-black  "
-        onClose={close} 
+        onClose={close}
       >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto ">
+        <DialogBackdrop className="fixed inset-0 backdrop-blur-sm" />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto  ">
           <div className="flex min-h-full items-center justify-center p-4 ">
             <DialogPanel
               transition
               className="w-full max-w-md rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 border-2"
             >
               {title && (
-                <DialogTitle
-                  as="h3"
-                  className="font-medium text-xl"
-                >
+                <DialogTitle as="h3" className="font-medium text-xl">
                   {title}
                 </DialogTitle>
               )}
 
-              <div className="mt-4">
-                {children}  
-              </div>
+              <div className="mt-4">{children}</div>
             </DialogPanel>
           </div>
         </div>
